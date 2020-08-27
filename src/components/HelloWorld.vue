11d0f7c48770406e4127ea85c092bd7d3fcf2223
https://github.com/ahmedelfikii/eCommerce.git <hemozghanim97@gmail.com>
2020-08-27 11:08:27 +0200

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,181 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="container">
+      <h1>ToDo List</h1>
+      <br />
+      <div class="container">
+        <form class="form-group">
+          <label class="w-10">Email address</label>
+          <input type="text" class="form-control w-8" v-model="text" />
+          <button
+            type="submit"
+            @click.prevent="addTodo(text)"
+            class="btn btn-primary"
+          >
+            Add
+          </button>
+        </form>
+        <div>
+          <transition-group name="fade" tag="ul">
+            <div
+              class="card wid-85 m-auto bg-light"
+              :class="{ 'border-success': finishColor }"
+              v-for="(item, index) in todos"
+              :key="index"
+            >
+              <div class="card-body">
+                <div class="row">
+                  <div
+                    class="col col-7  d-flex justify-content-center align-items-center"
+                  >
+                    <p
+                      class="marg-bottom lead text-center"
+                      :class="{ finishTask: lineFinishClass }"
+                      v-if="forEdit !== index"
+                    >
+                      {{ item }}
+                    </p>
+                    <input
+                      type="text"
+                      class="form-control w-4"
+                      v-model="textEdited"
+                      v-else-if="forEdit == index"
+                      @click.prevent="forEnable"
+                      :class="{ 'border-danger': error }"
+                    />
+                  </div>
+                  <div class="col col-5">
+                    <div
+                      class="btn btn-warning mr-2 ml-2"
+                      v-if="showButton"
+                      @click.prevent="close(index)"
+                    >
+                      <i class="fas fa-window-close text-white"></i>
+                    </div>
+                    <div
+                      class="btn btn-success mr-2 ml-2"
+                      v-if="showButton"
+                      :disabled="validated == 1"
+                      :class="{ disabled: disableClass }"
+                      @click.prevent="done(index)"
+                    >
+                      <i class="fas fa-check-square"></i>
+                    </div>
+                    <div
+                      class="btn btn-primary mr-2 ml-2"
+                      @click.prevent="editText(index)"
+                      v-if="showEditButton"
+                    >
+                      <i class="fas fa-edit"></i>
+                    </div>
+                    <div
+                      class="btn btn-danger mr-2 ml-2"
+                      @click.prevent="removeTask(index)"
+                    >
+                      <i class="fas fa-trash"></i>
+                    </div>
+                    <div
+                      class="btn btn-success mr-2 ml-2"
+                      @click.prevent="finishTask"
+                      v-if="doneTask"
+                    >
+                      Done
+                    </div>
+                    <div
+                      class="btn btn-success mr-2 ml-2"
+                      @click.prevent="UnFinishTask"
+                      v-else
+                    >
+                      Un-Done
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </transition-group>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+require("vue2-animate/dist/vue2-animate.min.css");
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data() {
+    return {
+      text: "",
+      todos: [],
+      forEdit: '',
+      textEdited: "",
+      showButton: false,
+      validated: 0,
+      disableClass: true,
+      showEditButton: true,
+      lineFinishClass: false,
+      finishColor: false,
+      doneTask: true,
+      error: false,
+      // editIndex: null
+    };
+  },
+  methods: {
+    addTodo(alltext) {
+      if (this.text === "") {
+        return;
+      } else {
+        alltext = this.text;
+        this.todos.push(alltext);
+        this.text = "";
+      }
+    },
+    editText(index) {
+      this.showButton = true;
+      this.forEdit = index;
+    },
+    forEnable() {
+      this.validated = 1;
+      return (this.disableClass = false);
+    },
+    done(index) {
+      if (this.validated == 0) {
+        return;
+      } else {
+        if (this.textEdited == "") {
+          this.error = true;
+        } else {
+          this.forEdit = '';
+          this.showButton = false;
+          this.todos.splice(index, 1, this.textEdited);
+        }
+      }
+      this.textEdited = "";
+    },
+    close() {
+      this.forEdit = '';
+      this.showButton = false;
+    },
+    finishTask() {
+      this.lineFinishClass = true;
+      this.showEditButton = false;
+      this.finishColor = true;
+      this.doneTask = false;
+    },
+    UnFinishTask() {
+      this.lineFinishClass = false;
+      this.showEditButton = true;
+      this.finishColor = false;
+      this.doneTask = true;
+    },
+    // isDisabled() {
+    //   return !this.validated;
+    // },
+    removeTask(index) {
+      return this.todos.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -54,5 +193,28 @@ li {
 }
 a {
   color: #42b983;
+}
+.w-10 {
+  width: 100%;
+  display: block;
+}
+.w-8 {
+  width: 80%;
+  display: inline-block !important;
+  margin-right: 10px;
+}
+.wid-85 {
+  width: 85%;
+  margin-bottom: 10px;
+}
+.marg-bottom {
+  margin-bottom: 0px;
+  margin-top: 0px;
+}
+.w-4 {
+  width: 40%;
+}
+.finishTask {
+  text-decoration: line-through;
 }
 </style>
